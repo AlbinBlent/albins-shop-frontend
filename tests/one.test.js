42a83457f913a4c7cdf0a521949911382a5b0a46
Albin Blent <albin.blent@gamil.com>
2016-10-27 14:57:44 +0200

    test

--- tests/one.test.js
+++ tests/one.test.js
@@ -7,7 +7,7 @@ describe('calculator', function () {
 		expect(calculator.sum(1 , 2)).toBe(3);
 	});
 
-	it('2 + 2 should equal 3', function() {
-		expect(calculator.sum(2 , 2)).toBe(3);
+	it('2 + 2 should equal 4', function() {
+		expect(calculator.sum(2 , 2)).toBe(4);
 	});
 });
