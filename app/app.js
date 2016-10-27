9509a3a3cbe4b4edc173fc3dd9bad3689fd840eb
Albin Blent <albin.blent@gamil.com>
2016-10-27 15:10:20 +0200

    added rest call to backend

--- app/app.js
+++ app/app.js
@@ -3,3 +3,11 @@ var calculator = {
         return a + b;
     }
 };
+
+$(document).ready(function() {
+    $.ajax({
+        url: "http://albinsshop.albins-shop.3d7a7a7f.svc.dockerapp.io:8080/product"
+    }).then(function(data) {
+        console.log("from rest call" + data);
+    });
+});
