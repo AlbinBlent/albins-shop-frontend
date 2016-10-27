5aeaa18defdfa745f34f200cab6a141d220eb9ae
Albin Blent <albin.blent@gamil.com>
2016-10-27 15:35:02 +0200

    added jquery

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+	'node_modules/jquery/dist/jquery.js',
       'tests/*.test.js',
       'app/*.js'
     ],
