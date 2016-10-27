c124ab52f045c0d7d15d4f334ddceb930a6e77b8
Albin Blent <albin.blent@gamil.com>
2016-10-27 13:23:50 +0200

    Jenkinsfile good

--- tests/one.test.js
+++ tests/one.test.js
@@ -2,4 +2,8 @@ describe('calculator', function () {
 	it('1 + 1 should equal 2', function() {
 		expect(calculator.sum(1 , 1)).toBe(2);
 	});
+
+	it('1 + 2 should equal 3', function() {
+		expect(calculator.sum(1 , 1)).toBe(3);
+	});
 });
