5fc67389746a108453a35dfe2b703408acf0272e
Albin Blent <albin.blent@gamil.com>
2016-10-27 16:18:22 +0200

    commit

--- app/app.js
+++ app/app.js
@@ -5,9 +5,26 @@ var calculator = {
 };
 
 $(document).ready(function() {
+    console.log("doc ready");
+    /*
     $.ajax({
-        url: "http://albinsshop.albins-shop.3d7a7a7f.svc.dockerapp.io:8080/product"
+        url: "http://albinsshop.albins-shop.3d7a7a7f.svc.dockerapp.io:8080/product",
+        dataType: 'jsonp'
     }).then(function(data) {
         console.log("from rest call" + data);
+    });
+    */
+
+    $.ajax({
+        url:"http://albinsshop.albins-shop.3d7a7a7f.svc.dockerapp.io:8080/product",
+        dataType: 'jsonp', // Notice! JSONP <-- P (lowercase)
+        success:function(json){
+            // do stuff with json (in this case an array)
+            alert("Success");
+        },
+        error:function(e){
+            console.log(e)
+            alert("Error" + e);
+        }
     });
 });
